21abbc933f49b7a4a37fdd40a705017dbb53703b
Ian <onyangoian71@gmail.com>
2021-09-01 20:51:25 +0300

    Add right bar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -4,7 +4,66 @@ function Rightbar() {
 
      <div className="right">
 
-    right
+    <div className="rightWrapper">
+    <div className="birthdayContainer"> 
+    <img className="birthdayImg" src="assets/gift.jpeg" />
+    <span className="birthdayText"> Ian and 4 others have birthay </span>
+    </div>
+    <img className="rightAdd" src="assets/car.jpg" />
+    <h4 className="rightText"> Online </h4>
+    <ul className="friendList">
+
+    <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+    </li>
+
+     <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+    </li>
+
+     <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+    </li>
+
+     <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+    </li>
+
+     <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+
+    </li>
+
+     <li className="friend"> 
+    <div className="profileImg">
+    <img className="profileImage" src="/assets/person/4.jpg" />
+    <span className="rightBagde"> </span>
+    </div>
+    <span className="profileName"> Joe </span>
+
+    </li>
+    </ul>
+     </div>
     </div>
 
     )
